39505e6e173768a68026b80e850973a4fa5112a4
ae9is <125031666+ae9is@users.noreply.github.com>
2026-04-03 07:38:49 +0000

    fix: cypress component tests for dependency updates

--- packages/fontpicker/cypress.config.ts
+++ packages/fontpicker/cypress.config.ts
@@ -8,6 +8,22 @@ export default defineConfig({
     devServer: {
       framework: 'react',
       bundler: 'vite',
+      viteConfig: {
+        plugins: [
+          {
+            name: 'remove-cypress-esbuild-plugin',
+            configResolved(config: any) {
+              const plugins = config.optimizeDeps?.esbuildOptions?.plugins
+              if (plugins) {
+                const index = plugins.findIndex((p: any) => p.name === 'cypress-esbuild-plugin')
+                if (index !== -1) {
+                  plugins.splice(index, 1)
+                }
+              }
+            },
+          },
+        ],
+      },
     },
     specPattern: 'src/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: 'cypress/support/component.ts',
@@ -17,8 +33,8 @@ export default defineConfig({
     specPattern: 'cypress/e2e/*.cy.{js,jsx,ts,tsx}',
     supportFile: 'cypress/support/e2e.ts',
     experimentalStudio: true,
-//    setupNodeEvents(on, config) {
-//      // implement node event listeners here
-//    },
+    //    setupNodeEvents(on, config) {
+    //      // implement node event listeners here
+    //    },
   },
 })
